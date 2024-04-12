129186ed57bcd1e71fb60bcd99fdcf3c4b19c9ec
Renuka kelkar <renuvkelkar@gmail.com>
2024-04-12 14:30:33 +0300

    updated the title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Codemagic Flutter Demo with trigger',
+      title: 'Codemagic Flutter Demo with webhook',
       theme: ThemeData(
         // This is the theme of your application.
         //
